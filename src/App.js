46019e19b02e47429de3ea82c716cff76bb64e6d
xuperxonic <xuperxonic12@gmail.com>
2022-08-22 11:01:47 +0900

    개발 시작-2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code>를 수정하세요.
         </p>
         <a
           className="App-link"
